854e609d8c338374f241736f8be84a895fe50c16
AyilaJyothi <119840130+AyilaJyothi@users.noreply.github.com>
2023-08-07 09:56:46 +0530

    import java.util.Scanner; public class Multi{     public static void main(String[] args){         Scanner sc=new Scanner(System.in);         int a,b,m;         a=sc.nextInt();         b=sc.nextInt();         m=a*b;         System.out.printf("%d",m);     } }

---
+++ Multiplication.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+public class Multi{
+    public static void main(String[] args){
+        Scanner sc=new Scanner(System.in);
+        int a,b,m;
+        a=sc.nextInt();
+        b=sc.nextInt();
+        m=a*b;
+        System.out.printf("%d",m);
+    }
+}
