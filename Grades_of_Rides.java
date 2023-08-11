5daf9fd399cdef28384d90047f8acd6bfb54930d
AyilaJyothi <119840130+AyilaJyothi@users.noreply.github.com>
2023-08-11 09:52:36 +0530

    import java.util.Scanner; public class Sample{     public static void main(String[] args){         Scanner sc=new Scanner(System.in);         int a,b,c;         a=sc.nextInt();         b=sc.nextInt();         c=sc.nextInt();         if(a>50 && b>60 && c>100)         {             System.out.print("10");         }         else if(a>50 && b>60)         {             System.out.print("9");         }         else if(b>60 && c>100)         {             System.out.print("8");         }         else if(a>50 && c>100)         {             System.out.print("7");         }         else if(a>50 || b>60 || c>100)         {             System.out.print("6");         }         else         {             System.out.print("5");         }     } }

---
+++ Grades_of_Rides.java
@@ -0,0 +1,34 @@
+import java.util.Scanner;
+public class Sample{
+    public static void main(String[] args){
+        Scanner sc=new Scanner(System.in);
+        int a,b,c;
+        a=sc.nextInt();
+        b=sc.nextInt();
+        c=sc.nextInt();
+        if(a>50 && b>60 && c>100)
+        {
+            System.out.print("10");
+        }
+        else if(a>50 && b>60)
+        {
+            System.out.print("9");
+        }
+        else if(b>60 && c>100)
+        {
+            System.out.print("8");
+        }
+        else if(a>50 && c>100)
+        {
+            System.out.print("7");
+        }
+        else if(a>50 || b>60 || c>100)
+        {
+            System.out.print("6");
+        }
+        else 
+        {
+            System.out.print("5");
+        }
+    }
+}
